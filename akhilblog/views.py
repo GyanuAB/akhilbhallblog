fdd61777b52fce15a6640a8688f4c256bb26c739
akhilbhall <akhilbhall123@.com>
2020-02-13 01:07:22 +0530

    Modified templates to display posts from database.

--- akhilblog/views.py
+++ akhilblog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'akhilblog/home.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'akhilblog/home.html', {'posts': posts})
+
