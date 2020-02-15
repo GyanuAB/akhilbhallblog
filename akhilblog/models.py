8ddc7095a444c1133bcdec508aad33422306ef59
akhilbhall <akhilbhall123@.com>
2020-02-15 11:57:14 +0530

    Added view and template for detailed blog post as well as CSS for the site

--- akhilblog/models.py
+++ akhilblog/models.py
@@ -16,4 +16,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
         
--- akhilblog/views.py
+++ akhilblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,6 @@ def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'akhilblog/home.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'akhilblog/post_detail.html', {'post': post})
